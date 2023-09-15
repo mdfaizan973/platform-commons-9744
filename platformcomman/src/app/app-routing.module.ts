95a206e6711bd3844a5358572c72bea0ebebca00
mdfaizan973 <faizan.md9735@gmail.com>
2023-09-15 19:00:21 +0530

    able to make routes

--- platformcomman/src/app/app-routing.module.ts
+++ platformcomman/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+
+// const routes: Routes = [];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CatalogueComponent } from './catalogue/catalogue.component';
+import { CartComponent } from './cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'catalogue', component: CatalogueComponent },
+  { path: 'cart', component: CartComponent },
+  { path: '', redirectTo: '/catalogue', pathMatch: 'full' }, // Default route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- platformcomman/src/app/app.module.ts
+++ platformcomman/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CatalogueComponent } from './catalogue/catalogue.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatalogueComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
