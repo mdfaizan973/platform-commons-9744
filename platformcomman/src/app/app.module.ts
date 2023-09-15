52505f1dd86e1eb6a5ad0d99ba0324f24db37169
mdfaizan973 <faizan.md9735@gmail.com>
2023-09-15 20:40:11 +0530

    able to map the data

--- platformcomman/src/app/app.module.ts
+++ platformcomman/src/app/app.module.ts
@@ -1,22 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CatalogueComponent } from './catalogue/catalogue.component';
 import { CartComponent } from './cart/cart.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CatalogueComponent,
-    CartComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CatalogueComponent, CartComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- platformcomman/src/app/catalogue/catalogue.component.ts
+++ platformcomman/src/app/catalogue/catalogue.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-catalogue',
   templateUrl: './catalogue.component.html',
-  styleUrls: ['./catalogue.component.css']
+  styleUrls: ['./catalogue.component.css'],
 })
-export class CatalogueComponent {
+export class CatalogueComponent implements OnInit {
+  products: any;
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('../assets/products.json').subscribe((data) => {
+      this.products = data;
+      console.log('Products:', this.products);
+    });
+  }
 }
